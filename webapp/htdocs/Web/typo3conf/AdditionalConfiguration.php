f87d510644ca9d957600e3f10c6b2a102872705e
Stefan Regniet <s.regniet@techdivision.com>
2018-09-04 10:41:37 +0200

    Removed „Barcamp“ from Code to make it more generic.

--- webapp/htdocs/Web/typo3conf/AdditionalConfiguration.php
+++ webapp/htdocs/Web/typo3conf/AdditionalConfiguration.php
@@ -4,8 +4,8 @@ $GLOBALS['TYPO3_CONF_VARS']['BE']['installToolPassword'] = md5('joh316');
 
 $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'] = 'smtp';
 
-// open at barcamp.test:81
+// open at t3mail.test:81
 $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_smtp_server'] = 'imap:25';
 
-// open at barcamp.test:8080
+// open at t3mail.test:8080
 //$GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_smtp_server'] = 'mailhog:1025';
